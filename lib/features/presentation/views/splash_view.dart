cf47f2f490a301a996779909ad1b0bce766f66c0
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-02-12 18:49:10 +0300

    create onGererateRout Method for mange Routing The App

---
+++ lib/features/presentation/views/splash_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/presentation/views/widget/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  static const routeName = "/splash";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SplashViewBody(),
+      ),
+    );
+  }
+}
