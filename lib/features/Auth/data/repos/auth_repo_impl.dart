7b4ebc5ab45b098674e5ec555d2845db3079d0b3
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-02-26 22:26:33 +0300

    implement authrepo createuserwithemailand password

--- lib/features/Auth/data/repos/auth_repo_impl.dart
+++ lib/features/Auth/data/repos/auth_repo_impl.dart
@@ -1,11 +1,30 @@
 import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/errors/custom_throw_exceptions_firebase.dart';
 import 'package:fruits_hub/core/errors/faliure.dart';
+import 'package:fruits_hub/core/services/firebase_auth_services.dart';
+import 'package:fruits_hub/features/Auth/data/models/user_model.dart';
 import 'package:fruits_hub/features/Auth/domain/entities/user_entity.dart';
 import 'package:fruits_hub/features/Auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthServices firebaseAuthServices;
+
+  AuthRepoImpl({required this.firebaseAuthServices});
   @override
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password) {
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password, String name) async {
+    try {
+      var user = await firebaseAuthServices.createUserWithEmailAndPassword(email: email, password: password);
+      return right(
+        UserModel.fromFirebaseUser(user!),
+      );
+    } on CustomAuthException catch (e) {
+      return left(
+        ServerFailure(e.message),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure("An Error Occured Please Try Again Later"),
+      );
+    }
   }
 }
--- lib/features/Auth/domain/repos/auth_repo.dart
+++ lib/features/Auth/domain/repos/auth_repo.dart
@@ -4,5 +4,5 @@ import 'package:fruits_hub/core/errors/faliure.dart';
 import 'package:fruits_hub/features/Auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password);
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(String email, String password, String name);
 }
