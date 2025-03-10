666e54ef358e970ba2a986da00e0830d38980a79
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-03-10 14:31:28 +0300

    Create Abstract DataBase Service

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,9 @@
+import 'package:fruits_hub/core/services/data_base_service.dart';
+
+class FirestoreService implements DatabaseService {
+  @override
+  Future<void> addData(String path, Map<String, dynamic> data) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
